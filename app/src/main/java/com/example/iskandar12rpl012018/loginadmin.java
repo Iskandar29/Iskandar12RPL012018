bc1a94b497b832d3c6c8661638c3f8e19cfd1f87
muhamadarzakhidayatullah123 <mmarzak905@gmail.com>
2020-08-12 16:19:46 +0700

    Initial commit

---
+++ app/src/main/java/com/example/iskandar12rpl012018/loginadmin.java
@@ -0,0 +1,12 @@
+package com.example.iskandar12rpl012018;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class loginadmin extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loginadmin);
+    }
+}
